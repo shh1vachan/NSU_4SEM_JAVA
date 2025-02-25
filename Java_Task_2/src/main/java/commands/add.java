a44d5b4e223530c2a487b54b7af09a5b063e0361
Ivan Sharapov <151162152+shh1vachan@users.noreply.github.com>
2025-02-25 23:13:50 +0700

    Update add.java

--- Java_Task_2/src/main/java/commands/add.java
+++ Java_Task_2/src/main/java/commands/add.java
@@ -14,11 +14,10 @@ public class add implements command {
             throw new IllegalArgumentException("Add command requires at least two values in the stack.");
         }
 
-        double b = context.get_stack().pop();
-        double a = context.get_stack().pop();
-        double result = a + b;
+        double second = context.get_stack().pop();
+        double first = context.get_stack().pop();
 
-        context.get_stack().push(result);
-        logger.info("ADD executed: {} + {} = {}", a, b, result);
+        context.get_stack().push(first + second);
+        logger.info("ADD executed: {} + {} = {}", first, second, first + second);
     }
 }
