4a049c657cb6b5d591ec12a5d29bb20b9117063d
Ivan Sharapov <151162152+shh1vachan@users.noreply.github.com>
2025-02-27 01:45:31 +0700

    Delete Java_Task_2 directory

--- Java_Task_2/src/main/java/commands/command.java
+++
@@ -1,9 +0,0 @@
-package commands;
-
-import context.execution_context;
-import java.util.List;
-
-public interface command
-{
-    void execute(execution_context context, List<String> args) throws Exception;
-}
