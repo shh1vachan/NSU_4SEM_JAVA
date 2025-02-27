62c31b0a0cdaeee475f8f609931bb038bedbcf43
Ivan Sharapov <151162152+shh1vachan@users.noreply.github.com>
2025-02-28 01:26:00 +0700

    minor changes | added tests

--- Java_Task_2/src/main/java/mainclass/Main.java
+++ Java_Task_2/src/main/java/mainclass/Main.java
@@ -10,9 +10,7 @@ import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Scanner;
 
-
-public class Main 
-{
+public class Main {
     private static final Logger logger = CalcLogger.getLogger();
 
     public static void main(String[] args)
@@ -46,7 +44,6 @@ public class Main
                 System.exit(1);
             }
         }
-            
         else
         {
             Scanner scanner = new Scanner(System.in);
