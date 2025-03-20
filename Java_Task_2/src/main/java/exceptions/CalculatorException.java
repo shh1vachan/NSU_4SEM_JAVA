73d1c409fd519247e23dce832ebf07c94329ef4e
Ivan Sharapov <151162152+shh1vachan@users.noreply.github.com>
2025-03-21 02:15:32 +0700

    added new exception hierarchy

---
+++ Java_Task_2/src/main/java/exceptions/CalculatorException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class CalculatorException extends Exception {
+    public CalculatorException(String message) {
+        super(message);
+    }
+
+    public CalculatorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
